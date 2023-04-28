91466dc61b9c4e91d7ced9fdae5cb47e348b590b
BigCatButt <itora@gmu.edu>
2023-04-28 13:42:38 -0400

    update agent.java

--- src/Agent.java
+++ src/Agent.java
@@ -32,10 +32,22 @@ public class Agent extends Employee {
 
     //mutators
     public void setRadioNum(String radioNum){
+        //make sure the length is 5 digits
+        if(radioNum.length() != 5) {
+            throw new IllegalArgumentException("Radio Number must be 5 digits long!");
+        } else if(!radioNum.matches("[0-9]+")) {
+            throw new IllegalArgumentException("Radio Number must be numeric!");
+        }
         this.radioNum = radioNum;
     }
 
     public void setAssignNum(String assignNum){
+        //make sure the length is 5 digits
+        if(assignNum.length() != 5) {
+            throw new IllegalArgumentException("Assignment Number must be 5 digits long!");
+        } else if(!assignNum.matches("[0-9]+")) {
+            throw new IllegalArgumentException("Assignment Number must be numeric!");
+        }
         this.assignNum = assignNum;
     }
 
